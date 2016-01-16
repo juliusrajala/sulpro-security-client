4ed2312b5b2586eb22d97eeaba1077ef2fdc53ca
Julius Rajala <juliusrajala@gmail.com>
2016-01-16 11:43:43 +0200

    Added twilio-key integration and app initialization.

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 #-*- coding: utf-8 -*-
+import settings as keys
 
 class app(object):
-  def __init__:
+  def __init__(self):
     #Blah blah
-    print "Something"
-    self.active = true
+    print "Initializing app class"
+    self.active = True
+    try:
+      self.twilio_id = keys.twilio_sid
+      self.twilio_secret=keys.twilio_secret
+      print "Keys initialized"
+    except:
+      print "Key initialization failed, closing application"
+      self.active = False
 
-  def main():
+  def main(self):
     #This will be where the main loop lives.
     while self.active:
-      self.active = false
+      self.active = False
+      print "Running the while loop"
 
 
 if __name__ == '__main__':
-  main()
+  ourApp = app()
+  ourApp.main()
