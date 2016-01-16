1bd98d88b6ce5da14f9f8e199697f59970b38de3
Julius Rajala <juliusrajala@gmail.com>
2016-01-16 11:05:33 +0200

    Initialized repository.

---
+++ app.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+class app(object):
+  def __init__:
+    #Blah blah
+    print "Something"
+    self.active = true
+
+  def main():
+    #This will be where the main loop lives.
+    while self.active:
+      self.active = false
+
+
+if __name__ == '__main__':
+  main()
---
+++ receiver.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+class receiver(object):
+  def __init__:
+    print "Initializing"
+
+  def main(self):
+    print "Doing things."
