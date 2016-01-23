334156771f2787343088b392fb3f7affd3780c73
Konemiees <konsta.sinisalo@gmail.com>
2016-01-23 16:12:38 +0200

    Hopefully functioning listener thread

--- serial_listener.py
+++ serial_listener.py
@@ -1,12 +1,41 @@
 import serial
+from threading import Thread
 
-tester = serial.Serial('COM19', 9600)
+class serial_listener(Thread):
 
-heard = 0
+	
 
-while True:
-	#heard = tester.readline()
-	print tester.readline()
-	if heard == 1:
-		print heard
+	def __init__(self, callback):
+		self.heard = 0
+		self.tester = serial.Serial('COM19', 9600)
+		self.is_alert = 0
+		self.callback = callback
+
+
+
+	def run(self):
+		self.listen_now()
+
+
+	def listen_now(self):
+
+		while True:
+			self.heard = self.tester.readline()
+			
+			if self.heard == 1:
+				self.is_alert = self.is_alert +1
+
+			if self.heard != 1:
+				self.is_alert = 0
+
+			if self.is_alert >= 5:
+				self.alert_on()
+
+
+
+	def alert_on(self):
+		self.callback()
+
+
+		
 	
